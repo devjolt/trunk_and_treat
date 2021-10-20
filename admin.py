0760350f63ff042ea2caa21eae43ee9932a26d02
devjolt <tom.m.warburton@gmail.com>
2021-10-20 10:07:50 +0100

    dash bug fixed and admin added

--- admin.py
+++ admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Trunk, Room, Soup, Activity
 
+admin.site.register(Trunk)
+admin.site.register(Room)
+admin.site.register(Soup)
+admin.site.register(Activity)
 # Register your models here.
